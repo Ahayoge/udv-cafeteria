0945ca65ae9c4eb75c082fb21a4e70ac52f77141
Ahayoge <semnes1912@mail.ru>
2024-09-18 19:02:21 +0500

    Первичная настройка
    Подготовил репозиторий для дальнейшей работы. Подключил tailwind, добавил шрифты и цвета из брендбука. Установил библиотеки, которые будут использованы в дальнейшем

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="font-golos text-3xl font-bold text-mint-100">HELLO WORLD!</h1>
+      <h2>Проверка шрифта<br />проверка интерлиньяжа</h2>
+    </>
+  );
+}
