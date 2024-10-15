8fc0b9f10a0b99d055e7111bdd4561575d5c4d94
Ahayoge <semnes1912@mail.ru>
2024-10-15 11:18:27 +0500

    Изменил структуру

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
-export default function App() {
-    return (
-        <>
-        
-        </>
-    );
-}
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './Normalize.css';
+import './App.css';
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <header>
+            <p>HELLO</p>
+        </header>
+    </StrictMode>
+);
